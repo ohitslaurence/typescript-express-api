677ee96b4aa688a8055bbe1e917c28dd9a04621a
ljdavies <laurence.davies@libryo.com>
2020-05-24 11:59:28 +0200

    Initial project configuration

---
+++ src/config/index.ts
@@ -0,0 +1,39 @@
+import { merge } from 'lodash';
+const env = process.env.NODE_ENV || 'development';
+
+export interface IConfiguration {
+  env: string;
+  isDev: boolean;
+  isTest: boolean;
+  port: number;
+  secrets: Record<string, unknown>;
+  dbUrl: string;
+}
+const baseConfig: IConfiguration = {
+  env,
+  isDev: env === 'development',
+  isTest: env === 'testing',
+  port: 3000,
+  secrets: {
+    jwt: process.env.JWT_SECRET,
+    jwtExp: '100d',
+  },
+  dbUrl: '',
+};
+
+let envConfig = {};
+
+switch (env) {
+  case 'dev':
+  case 'development':
+    envConfig = require('./dev').config;
+    break;
+  case 'test':
+  case 'testing':
+    envConfig = require('./testing').config;
+    break;
+  default:
+    envConfig = require('./dev').config;
+}
+
+export default merge(baseConfig, envConfig);
---
+++ src/server.ts
@@ -0,0 +1,41 @@
+import express, { Request, Response, Express } from 'express';
+import { json, urlencoded } from 'body-parser';
+import morgan from 'morgan';
+import config from './config';
+import cors from 'cors';
+// import { signup, signin, protect } from './utils/auth';
+import { connect } from './utils/db';
+
+export const app: Express = express();
+
+app.disable('x-powered-by');
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send({ message: 'Hello' });
+});
+
+app.post('/', (req: Request, res: Response) => {
+  console.log(req.body);
+  res.send({ message: 'Ok' });
+});
+
+// app.post('/signup', signup);
+// app.post('/signin', signin);
+
+// app.use('/api', protect);
+
+export const start = async (): Promise<void> => {
+  try {
+    await connect();
+    app.listen(config.port, () => {
+      console.log(`REST API on http://localhost:${config.port}/api`);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
